6bf29b62566e9e17d60f5e153c508e4c94c7fdde
Yvan <hzyvan@gmail.com>
2024-03-11 18:58:09 +0800

    1-30 answers

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(nums: i32) -> i32 {
+    if nums == 35 || nums == 40 {
+        2 * nums
+    } else {
+        1 * nums
+    }
+}
 
 // Don't modify this function!
 #[test]
